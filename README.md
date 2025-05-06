92add06d437d03c6d0b54d956ef75fbae7bfae6e
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-05-06 21:47:11 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -8,8 +8,21 @@ This project is a starting point for a Flutter application.
 
 # Day 1
 
-How to create flutter app.
-How to use MaterialApp in flutter project
-What is scaffold 
-How to add appBar
-How to add image in our flutter project
+How to create flutter app?
+
+How to use MaterialApp in flutter project?
+return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red.shade200,
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(193, 56, 54, 1),
+          title: Text('Project'),
+        ),
+        body: Center(child: Image(image: AssetImage('assets/Anshu.jpg'))),
+      ),
+    );
+}
+What is scaffold ?
+How to add appBar?
+How to add image in our flutter project?
+How to use Statelesswidget?
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.red.shade200,
+//         appBar: AppBar(
+//           backgroundColor: const Color.fromRGBO(193, 56, 54, 1),
+//           title: Text('Project'),
+//         ),
+//         body: Center(child: Image(image: AssetImage('assets/Anshu.jpg'))),
+//       ),
+//     ),
+//   );
+// }
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red.shade200,
         appBar: AppBar(
           backgroundColor: const Color.fromRGBO(193, 56, 54, 1),
           title: Text('Project'),
         ),
-        body: Center(child: Image(image: AssetImage('assets/Anshu.jpg'))),
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.amber,
+            child: Center(child: Text('Container')),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
