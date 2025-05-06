adfc79919881266e1b87ebb4f8cd34bdd343f9f1
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-05-06 20:54:52 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,11 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red.shade200,
-        appBar: AppBar(backgroundColor: Colors.red.shade400, title: Text('Project')),
-        body: Center(child: Image(image: NetworkImage('https://th.bing.com/th/id/OIP.b4I7OSQXr2NoQFlShVth_QHaE8?cb=iwc1&rs=1&pid=ImgDetMain'))),
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(193, 56, 54, 1),
+          title: Text('Project'),
+        ),
+        body: Center(child: Image(image: AssetImage('assets/Anshu.jpg'))),
       ),
     ),
   );
