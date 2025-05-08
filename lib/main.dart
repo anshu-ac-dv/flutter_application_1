21ab204f60b3d54a4d8c50759b01f5a60a63aa36
Anshu Kumar <anshu.ac.dv@gmail.com>
2025-05-08 12:25:42 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ import 'package:flutter/material.dart';
 //   );
 // }
 // void main() {
-//   runApp(NewApp());
+//   runApp(MyApp());
 // }
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
@@ -41,6 +41,55 @@ import 'package:flutter/material.dart';
 //     );
 //   }
 // }
+// void main() {
+//   runApp(NewApp());
+// }
+
+// class NewApp extends StatelessWidget {
+//   const NewApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.red.shade200,
+//         appBar: AppBar(
+//           backgroundColor: Colors.amber.shade900,
+//           title: Text('This is Flutter Project'),
+//         ),
+//         body: SafeArea(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.start,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Container(
+//                 height: 100,
+//                 width: 100,
+//                 color: Colors.blue,
+//                 child: Center(child: Text('This is blue Container')),
+//               ),
+//               SizedBox(height: 100),
+//               Container(
+//                 height: 100,
+//                 width: 100,
+//                 color: Colors.yellow,
+//                 child: Center(child: Text('This is Yellow Container')),
+//               ),
+//               SizedBox(height: 100),
+//               Container(
+//                 height: 100,
+//                 width: 100,
+//                 color: Colors.pink,
+//                 child: Center(child: Text('This is Pink Container')),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 void main() {
   runApp(NewApp());
 }
@@ -55,11 +104,15 @@ class NewApp extends StatelessWidget {
         backgroundColor: Colors.red.shade200,
         appBar: AppBar(
           backgroundColor: Colors.amber.shade900,
-          title: Text('This is Flutter Project'),
+          title: Text(
+            'This is Flutter Project',
+            style: TextStyle(fontFamily: 'Pacifico', fontSize: 50),
+          ),
         ),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 height: 200,
@@ -67,12 +120,14 @@ class NewApp extends StatelessWidget {
                 color: Colors.blue,
                 child: Center(child: Text('This is blue Container')),
               ),
+              SizedBox(width: 100),
               Container(
                 height: 200,
                 width: 200,
                 color: Colors.yellow,
                 child: Center(child: Text('This is Yellow Container')),
               ),
+              SizedBox(width: 100),
               Container(
                 height: 200,
                 width: 200,
